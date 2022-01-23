9c258a958f7080dc9b2ef39e0201399ac7757fed
matteomeier <matteogianluca.meier@web.de>
2022-01-23 10:17:52 +0100

    add regression analysis

--- scripts/aggregate_cluster_to_categorize.py
+++ scripts/aggregate_cluster_to_categorize.py
@@ -1,5 +1,14 @@
 import pandas as pd
 
 df = pd.read_json('../../data/BTW17_Suggestions/suggestions/cluster.json')
-output = df[['cluster','suggestion']].groupby(['cluster'])['suggestion'].sum()
-output.to_csv(('../../data/BTW17_Suggestions/suggestions/cluster_categories.csv'))
+
+df['sugg'] = df['suggestion'].apply(lambda x: ' '.join([sugg for sugg in x]))
+
+output = {'cluster':[],'sugg':[]}
+
+for cluster in df['cluster'].unique():
+    output['cluster'].append(cluster)
+    tmp = df[df['cluster']==cluster]
+    output['sugg'].append(tmp['sugg'].str.cat(sep=', '))
+
+pd.DataFrame(data=output).to_csv(('../../data/BTW17_Suggestions/suggestions/cluster_categories.csv'))
